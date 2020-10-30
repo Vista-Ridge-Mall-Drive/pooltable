78619576a086ce99b40be1adc0dbd44b96f7c9ba
magnustiberius <bgonzales20001@gmail.com>
2020-10-29 23:28:42 -0500

    schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,83 +1,39 @@
-drop table if exists USERS;
-drop table if exists country;
 drop table if exists inventory;
-drop table if exists delivery_header;
-drop table if exists delivery_details;
-drop table if exists inventory_ledger_quantity;
-drop table if exists inventory_ledger_amount;
+drop table if exists pooltables;
+drop table if exists transaction;
 
-create table USERS(
-  ID int not null AUTO_INCREMENT,
-  NAME varchar(100) not null,
-  STATUS int,
-  PRIMARY KEY ( ID )
-);
-
-CREATE TABLE country (
-  id   INTEGER      NOT NULL AUTO_INCREMENT,
-  name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (id)
-);
 
 CREATE TABLE inventory (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
   code 			VARCHAR(128) NOT NULL,
   name 			VARCHAR(128) NOT NULL,
   desc 			VARCHAR(128) NOT NULL,
-  unit_price 		FLOAT,
-  qty_in_stock 		FLOAT,
-  unit_weight 		VARCHAR(128),
-  reorder_level 	FLOAT,
-  reorder_time_in_days 	FLOAT,
-  reorder_qty   	FLOAT,
-  discontinued  	BOOL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE inventory_ledger_quantity (
-  id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  ref 			VARCHAR(128) NOT NULL,
-  debit 		FLOAT,
-  credit 		FLOAT,
-  post_date 	DATE,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE inventory_ledger_amount (
-  id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  ref 			VARCHAR(128) NOT NULL,
-  debit 		FLOAT,
-  credit 		FLOAT,
-  post_date 	DATE,
   PRIMARY KEY (id)
 );
 
 
-CREATE TABLE delivery_header (
+CREATE TABLE pooltables (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  vendor_id		INTEGER      NOT NULL,
-  vendor_name 	VARCHAR(128) NOT NULL,
-  receive_date 	DATE,
-  user_id		INTEGER      NOT NULL,
-  posted_flag	BOOL,
-  post_date 	DATE,
+  inventory_id  INTEGER,
+  status		INTEGER,
+  available		BOOLEAN,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE delivery_details (
+
+CREATE TABLE transaction (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  header_id		INTEGER      NOT NULL,
-  inventory_id	INTEGER      NOT NULL,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  desc 			VARCHAR(128) NOT NULL,
-  unit_price 	FLOAT,
-  unit_weight 	VARCHAR(128),
-  posted_flag	BOOL,
-  post_date 	DATE,
+  time_start	DATE,
+  time_end		DATE,
+  pooltable_id  INTEGER,
+  rack_id 		INTEGER,
+  order_id		INTEGER,
+  billing_id	INTEGER,
+  open_tab		BOOLEAN,
+  tab_name		VARCHAR(128),
+  close_tab		BOOLEAN,
   PRIMARY KEY (id)
+);
+
+
 );
